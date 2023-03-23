f39fbeb7bc275457260585d3150295fc34371969
pythonmaster <theone7877@gmail.com>
2023-03-23 22:44:27 +0200

    minor fixes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 import asyncio
 import requests
@@ -8,39 +9,51 @@ app = FastAPI(
     title="Scraper"
 )
 
-
-async def check_status():
-    response = requests.head("https://www.tesmanian.com/blogs/")
-    status_code = response.status_code
-    if status_code == requests.codes.ok:
-        print("Status code is 200. I can start scraping.")
-    else:
-        print(f"Status code is {status_code}, something went wrong.")
+REPEAT_TIME = 999
 
 
-async def save_cookie():
-    if os.path.exists("data/cookies.txt") and os.stat("data/cookies.txt").st_size != 0:
-        pass
-    else:
-        # Send a GET request to the website to retrieve the cookies
-        response = requests.get("https://www.tesmanian.com/blogs/tesmanian-blog?page=1")
-        # Save the cookies to a file
-        with open("data/cookies.txt", "w") as f:
-            for cookie in response.cookies:
-                f.write(f"{cookie.name}={cookie.value};")
-        print("Saved new cookies.")
+class Scraper(object):
+
+    async def check_status(self):
+        response = requests.head("https://www.tesmanian.com/blogs/tesmanian-blog")
+        status_code = response.status_code
+        if status_code == requests.codes.ok:
+            print("Status code is 200. I can start scraping.")
+            return True
+        else:
+            print(f"Status code is {status_code}, something went wrong.")
+            return False
 
 
-async def scrape_website():
-    # Load the saved cookies from the file
-    with open("data/cookies.txt", "r") as f:
-        cookie_str = f.read().strip()
-    cookies = {}
-    for cookie in cookie_str.split(";"):
-        cookie_parts = cookie.split("=")
-        if len(cookie_parts) == 2:
-            name, value = cookie_parts
-            cookies[name] = value
+    async def save_cookie(self):
+        if os.path.exists("data/cookies.txt") and os.stat("data/cookies.txt").st_size != 0:
+            pass
+        else:
+            # Send a GET request to the website to retrieve the cookies
+            response = requests.get("https://www.tesmanian.com/blogs/tesmanian-blog?page=1")
+            # Save the cookies to a file
+            with open("data/cookies.txt", "w") as f:
+                for cookie in response.cookies:
+                    f.write(f"{cookie.name}={cookie.value};")
+            print("Saved new cookies.")
+
+
+    async def open_cookie(self):
+        # Load the saved cookies from the file
+        with open("data/cookies.txt", "r") as f:
+            cookie_str = f.read().strip()
+        cookies = {}
+        for cookie in cookie_str.split(";"):
+            cookie_parts = cookie.split("=")
+            if len(cookie_parts) == 2:
+                name, value = cookie_parts
+                cookies[name] = value
+        return cookies
+
+
+async def scrape_website(scraper):
+    # scraper = Scraper()
+    cookies = await scraper.open_cookie()
 
     while True:
         response = requests.get("https://www.tesmanian.com/blogs/tesmanian-blog?page=1", cookies=cookies)
@@ -61,11 +74,22 @@ async def scrape_website():
             results.append({"title": title_h2, "link": link})
         print(results)
 
-        await asyncio.sleep(999)
+        await asyncio.sleep(REPEAT_TIME)
 
 
+@app.on_event("startup")
+async def startup_event():
+    status_ok = await Scraper().check_status()
+    if not status_ok:
+        raise Exception("Status code is not OK, stopping program")
+    await Scraper().save_cookie()
+    scraper = Scraper()
+    asyncio.create_task(scrape_website(scraper))
 
-@app.get("/")
-async def read_root():
-    asyncio.create_task(scrape_website())
-    return {"message": "Scraping Tesmanian blog for new posts every 15 seconds."}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port="8000",
+        reload=True)
